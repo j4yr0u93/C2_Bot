4c191d61d1acdcd14045a6f9939dac46f7b1e0a2
j4yr0u93 <lourie10@gmail.com>
2020-10-30 20:23:22 -0400

    toml

--- c2_bot.py
+++ c2_bot.py
@@ -18,4 +18,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('your token here')
+client.run(discon['secure']['TOKEN'])
