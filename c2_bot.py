b80ca8eb2195f091db1c0bb54e7f396ab5d586c1
j4yr0u93 <lourie10@gmail.com>
2020-10-31 19:18:25 -0400

    Update c2_bot.py

--- c2_bot.py
+++ c2_bot.py
@@ -1,5 +1,4 @@
 import asyncio
-import uvloop
 import discord
 
 from C2_Bot import __version__, discon
@@ -10,12 +9,21 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+#message user welcome message when they join server
+@client.event
+async def on_member_join(member):
+    if member.guild.id == discon['id']['SERVER']:
+        channel = client.get_channel(discon['id']['WELCOME'])
+        await channel.send(f'{member.mention}')
+
+
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    #ignore bots
+    if message.author.bot:
         return
 
-    if message.content.startswith('$hello'):
+    elif message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
 client.run(discon['secure']['TOKEN'])
