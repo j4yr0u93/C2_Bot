bf74c5f3ec2a3ef0341a33060684b30a8c5fa593
j4yr0u93 <lourie10@gmail.com>
2020-11-02 23:31:55 -0500

    testing author id

--- mods/help.py
+++ mods/help.py
@@ -9,9 +9,12 @@ async def echo(message):
     echo_content = " ".join(echo_raw[1:])
     await message.channel.send(echo_content)
 
+async def author_id(message):
+    await message.channel.send(message.author.id())
+
 async def monke(message):
     await message.channel.send('dev who made bot is monke')
 
 
-allowed = {'test' : test, 'echo' : echo}
+allowed = {'test' : test, 'echo' : echo, 'author_id' : author_id}
 secure = {'monke' : monke}
