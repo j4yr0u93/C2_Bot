27b83ee77f5b652368261a7e97679d6afb5a0118
j4yr0u93 <lourie10@gmail.com>
2020-11-02 20:12:06 -0500

    Update c2_bot.py

--- c2_bot.py
+++ c2_bot.py
@@ -39,14 +39,14 @@ async def on_message(message):
     if message.author.bot:
         return
     #run a command if prexix matches and first 'word' after prefixes is in 'commands'
-elif message.content.startswith(discon['options']['PREFIX']) and (message.content.split[0][len(discon['options']['PREFIX']):] in allowed_functions):
+elif message.content.startswith(discon['options']['PREFIX']) and (message.content.split()[0][len(discon['options']['PREFIX']):] in allowed_functions):
         try:
-            exec("{f}(args = {args}, message = {m})".format(f=message.content.split[0][len(discon['options']['PREFIX']):], args=message.content.split[1:], m=message))
+            exec("{f}(args = {args}, message = {m})".format(f=message.content.split()[0][len(discon['options']['PREFIX']):], args=message.content.split[1:], m=message))
         except Exception as e:
             print(e)
     elif message.content.startswith(discon['options']['PREFIX']) and (message.author.id == discon['options']['OWNER']):
         try:
-            exec("{f}(args = {args}, message = {m})".format(f=message.content.split[0][len(discon['options']['PREFIX']):], args=message.content.split[1:], m=message))
+            exec("{f}(args = {args}, message = {m})".format(f=message.content.split()[0][len(discon['options']['PREFIX']):], args=message.content.split[1:], m=message))
         except:
             await message.channel.send('bruh')
 
