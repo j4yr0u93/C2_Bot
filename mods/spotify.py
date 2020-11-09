5a93a5711b5dd23c5b3a45be8a1251cccd23799b
j4yr0u93 <lourie10@gmail.com>
2020-11-08 23:23:39 -0500

    Update spotify.py

--- mods/spotify.py
+++ mods/spotify.py
@@ -22,6 +22,7 @@ async def spotify_search(message, spotify_client = get_spotify_client()):
     search_raw = message.content.split()
     search_query= " ".join(search_raw[1:])
     search_results = spotify_client.search(search_query, limit=1)
+    print(search_results)
 #    result_url = search_results.get('external_urls')
     await message.channel.send(search_results)
 
